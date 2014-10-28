8d0402be6595ce918785bf6955121fbeb39862b1
Jason Frey <fryguy9@gmail.com>
2014-10-28 16:42:41 -0400

    Exit the specs with a clear message when the VDDK is not present at all.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,17 @@
 require 'minitest'
 require 'minitest/autorun'
-require 'ffi-vix_disk_lib'
+
+begin
+  require 'ffi-vix_disk_lib'
+rescue LoadError
+  STDERR.puts <<-EOMSG
+
+The VMware VDDK must be installed in order to run specs.
+
+The VMware VDDK is not redistributable, and not available on MacOSX.
+See https://www.vmware.com/support/developer/vddk/ for more information.
+
+EOMSG
+
+  exit 1
+end
