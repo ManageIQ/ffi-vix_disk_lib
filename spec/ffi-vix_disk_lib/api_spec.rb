512465ee1df2aa1876d72411c0debe62432b0299
Jason Frey <jfrey@redhat.com>
2020-01-13 14:15:19 -0500

    Simplify loading of the dynamic library and make it work on Mac

--- spec/ffi-vix_disk_lib/api_spec.rb
+++ spec/ffi-vix_disk_lib/api_spec.rb
@@ -1,17 +1,27 @@
 describe FFI::VixDiskLib::API do
-  let(:major_version) { described_class::VERSION_MAJOR }
-  let(:minor_version) { described_class::VERSION_MINOR }
-  let(:log)           { lambda { |_string, _pointer| } }
-  let(:lib_dir)       { nil }
+  let(:log)     { lambda { |_string, _pointer| } }
+  let(:lib_dir) { nil }
 
-  context "init" do
+  it "VERSION" do
+    expect(described_class::VERSION).to eq "6.7.0"
+  end
+
+  it "VERSION_MAJOR" do
+    expect(described_class::VERSION_MAJOR).to eq 6
+  end
+
+  it "VERSION_MINOR" do
+    expect(described_class::VERSION_MINOR).to eq 7
+  end
+
+  describe ".init" do
     it "initializes successfully" do
-      err = described_class.init(major_version, minor_version, log, log, log, lib_dir)
+      err = described_class.init(described_class::VERSION_MAJOR, described_class::VERSION_MINOR, log, log, log, lib_dir)
       expect(err).to eq(described_class::VixErrorType[:VIX_OK])
     end
   end
 
-  context "ConnectParams" do
+  describe "ConnectParams" do
     case described_class::VERSION
     when "6.5.0"
       it "has vimApiVer" do
